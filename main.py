a04f392b5926342dcde157055af289df7964d2c2
victorio-kalelle <kalelleeee@gmail.com>
2025-07-20 02:40:56 +0800

    add homepage.html

--- main.py
+++ main.py
@@ -5,4 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 
 def homepage():
-    return render_template()
+    return render_template('homepage.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
