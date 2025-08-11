5b9396ed4c6f88d2770f3103340239c6c7e1e14b
victorio-kalelle <kalelleeee@gmail.com>
2025-08-11 14:56:15 +0800

    rename files

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def homepage():
-    return render_template('homepage.html')
+def titlepage():
+    return render_template('titlepage.html')
 
 @app.route('/login')
 def loginpage():
