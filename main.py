69d72afde99942ae761fbf7030a2cf54396e5ca0
victorio-kalelle <kalelleeee@gmail.com>
2025-08-11 16:36:32 +0800

    add files

--- main.py
+++ main.py
@@ -15,8 +15,16 @@ def signuppage():
     return render_template('signup.html')
 
 @app.route('/layout')
-def photoboothpage():
+def layout_selection():
     return render_template('layout.html')
+
+@app.route('/frame')
+def frame_selection():
+    return render_template('frame.html')
+
+@app.route('/photobooth')
+def photobooth_page():
+    return render_template('photobooth.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
